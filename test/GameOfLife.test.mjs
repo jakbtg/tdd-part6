a4d0b455ee253c7199f89de0641dfd79c5bf5e72
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-29 23:19:02 +0200

    changing test for grid, because grid should be bigger than pattern

--- test/GameOfLife.test.mjs
+++ test/GameOfLife.test.mjs
@@ -7,6 +7,11 @@ describe("Game of Life tests", () => {
         let grid = new GameOfLife("patterns/glider.rle");
         expect(grid.getInitialGrid()).to.deep.equal(matrix);
     });
+
+    it("grid should be one cell bigger than the max of x or y of the pattern", () => {
+        let grid = new GameOfLife("patterns/block.rle");
+        expect(grid.getInitialGrid().length).to.equal(3);
+    });
 });
 
 // begin work with Game of Life rules
@@ -16,5 +21,13 @@ describe("Game of Life rules", () => {
         let grid = game.getInitialGrid();
         let neighbors = game.getNeighbors(grid, 1, 1);
         expect(neighbors).to.equal(5);
+    });
+
+    // First i probably need to make the matrix bigger and squared to test the edge cases
+    xit("should return the correct number of neighbors with another pattern", () => {
+        let game = new GameOfLife("patterns/block.rle");
+        let grid = game.getInitialGrid();
+        let neighbors = game.getNeighbors(grid, 0, 0);
+        expect(neighbors).to.equal(8);
     });
 });
