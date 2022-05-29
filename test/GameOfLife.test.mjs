2fda0b2e41129e2c3ae7e8382b8709773cb545f6
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-29 22:43:00 +0200

    tried same test with another pattern

--- test/GameOfLife.test.mjs
+++ test/GameOfLife.test.mjs
@@ -3,8 +3,8 @@ import { GameOfLife } from "../src/GameOfLife.mjs";
 
 describe("Game of Life tests", () => {
     it("can create a grid from a given pattern", () => {
-        let matrix = [[1, 1], [1, 1]];
-        let grid = new GameOfLife("patterns/block.rle");
+        let matrix = [[0, 1, 0], [0, 0, 1], [1, 1, 1]];
+        let grid = new GameOfLife("patterns/glider.rle");
         expect(grid.getInitialGrid()).to.deep.equal(matrix);
     });
 });
