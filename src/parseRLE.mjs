b892c76320c0efaf9199cdae418bb84de0e92c70
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-29 17:43:14 +0200

    refactor parseRLE

--- src/parseRLE.mjs
+++ src/parseRLE.mjs
@@ -1,4 +1,11 @@
 export function parseRLE(rle) {
-  let result = [[1, 1], [1, 1]];
-  return result;
+  let decoded = rle.slice(0, rle.length - 1);
+  decoded = decoded.replace(/(\d+)(\D)/g, function (match, num) {
+    return match.split(num)[1].repeat(num);
+  })
+  decoded = decoded.split("$");
+  decoded = decoded.map(row => row.replace(/o/g, "1").replace(/b/g, "0"));
+  decoded = decoded.map(row => row.split(""));
+  decoded = decoded.map(row => row.map(num => parseInt(num)));
+  return decoded;
 }
